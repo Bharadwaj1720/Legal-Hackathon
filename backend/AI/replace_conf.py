53eb5c89349e4f219a5a1070359c2a89ad1c964b
emilyta788 <emilyta788@gmail.com>
2025-04-06 13:46:25 -0400

    Updated replace_conf

--- backend/AI/replace_conf.py
+++ backend/AI/replace_conf.py
@@ -10,10 +10,16 @@ conf_words (array of confidential words), text (text file extracted from pdf)
 
 Returns: 
 clean_text (text words censored w/ asterisks)
+redacted_words (a log of redacted words from text)
 """
 
-def replace_conf(conf_words, text): 
+def replace_conf(conf_words, text):
+
+    redacted_words = []
+
     for words in conf_words:
         pattern = re.compile(re.escape(word), re.IGNORECASE)
+        if pattern.search(text):
+            redacted_words.append(word)
         clean_text = pattern.sub('*' * len(word), text)
-    return clean_text
+    return clean_text, redacted_words
